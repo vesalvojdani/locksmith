c55984421d75272ca92efe244f8e869debe8c8c6
Vesal Vojdani <vesal.vojdani@gmail.com>
2021-11-22 13:50:16 +0000

    Add readme to sv-comp dist

--- svcomp_dist.sh
+++ svcomp_dist.sh
@@ -5,7 +5,7 @@ DIST="dist/locksmith"
 mkdir -p $DIST
 cat locksmith.in | sed -e "s.ROOT.\.." > $DIST/locksmith
 chmod a+x $DIST/locksmith 
-cp LICENSE simple.c $DIST
+cp README.md LICENSE simple.c $DIST
 mkdir -p $DIST/cil/obj/x86_LINUX $DIST/cil/lib $DIST/cil/bin
 cp cil/obj/x86_LINUX/cilly.* $DIST/cil/obj/x86_LINUX
 cp cil/lib/*.pm $DIST/cil/lib
